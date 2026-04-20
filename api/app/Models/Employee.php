dc0d7460146355eb7be960fca354af1c9e82adae
kitokoh <kitokoh.africa@gmail.com>
2026-04-20 03:31:17 +0300

    style: remove redundant import in same namespace

--- api/app/Models/Employee.php
+++ api/app/Models/Employee.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\PersonalAccessToken;
 use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
